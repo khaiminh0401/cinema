8a1d203f54e878da232ecf2017541e3916fecf13
khaiminh0401 <85891822+khaiminh0401@users.noreply.github.com>
2023-07-09 01:47:34 +0700

    navbar

---
+++ app/(user)/page.tsx
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+import "./index.css";
+export default function Home() {
+  return (
+    <div className='main'>
+      <div className='hover'></div>
+    </div>
+  )
+}
---
+++ components/Navbar/index.tsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+
+const Navbar = () => {
+    return (
+        <nav className="navbar bg-opacity justify-content-around">
+            <Image src="/assert/img/logo.png" width={"150"} height={"50"} alt=""/>
+            <section className="d-flex justify-content-between align-items-center w-50">
+                <a className="nav-link fw-bold">Phim</a>
+                <a className="nav-link fw-bold">Lịch chiếu</a>
+                <a className="nav-link fw-bold">Liên hệ</a>
+                <a className="nav-link fw-bold">Về chúng tôi</a>
+                <button className="btn btn-danger">Đăng nhập</button>
+            </section>
+        </nav>
+    );
+}
+export default Navbar;
