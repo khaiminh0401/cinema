c0d40262be73d30edb063bf96e9295f05407b4c2
Khang Ho <hohoangkhang18@gmail.com>
2023-07-13 00:50:50 +0700

    registation form

---
+++ app/(user)/registration/page.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+import FormSignUp from "@/components/FormSignUp";
+
+const Registration = () => {
+  return (
+    <>
+      <FormSignUp/>
+    </>
+  )
+}
+
+export default Registration;
