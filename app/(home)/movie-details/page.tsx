e46c32ba2caa0569a286d59258198ef3ba16f538
khaiminh0401 <85891822+khaiminh0401@users.noreply.github.com>
2023-09-04 16:09:54 +0700

    change 5 item for carousel

--- app/(home)/movie-details/page.tsx
+++ app/(home)/movie-details/page.tsx
@@ -44,7 +44,7 @@ const MovieDetails = () => {
     const responsive = {
         desktop: {
             breakpoint: { max: 3000, min: 1024 },
-            items: 4,
+            items: 5,
         },
         tablet: {
             breakpoint: { max: 1024, min: 464 },
