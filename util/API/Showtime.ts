81868217d50dcfe7c26db1496640fec045249c5f
nguyensontrungkien <nguyentrungkien08010801@gmail.com>
2023-09-04 17:36:44 +0700

    showyime (đổi path)

--- util/API/Showtime.ts
+++ util/API/Showtime.ts
@@ -1,6 +1,6 @@
 import { fetchAPI } from "./axios";
 const findShowtimeByMovieAndDate = async (showdate: any, movieId: any) => {
-    return (await fetchAPI.get("/showtime/movieAndDate", { params: { showdate: showdate, movieId: movieId } })).data as showtime[];
+    return (await fetchAPI.get("/showtime/byDateOfMovie", { params: { showdate: showdate, movieId: movieId } })).data as showtime[];
 }
 
 export const showtimeAPI = {
