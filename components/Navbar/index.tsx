1f703e78b2b5aa5fd3ade91b3f5fbf7544f12b80
Thanh Sơn <thanhson9112@gmail.com>
2023-09-09 13:30:55 +0700

    Modify Navbar

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -4,24 +4,7 @@ import "./index.css";
 import { useSession } from "next-auth/react";
 import { SignOutButton } from "../authButtons";
 import Link from "next/link";
-
-const Specical = () => {
-    return (
-        <nav className="navbar bg-opacity justify-content-around">
-            <Image src="/assert/img/logo.png" width={"150"} height={"50"} alt="" />
-            <section className="d-flex justify-content-between align-items-center w-50">
-                <a className="nav-link fw-bold">Phim</a>
-                <a className="nav-link fw-bold">Lịch chiếu</a>
-                <a className="nav-link fw-bold">Liên hệ</a>
-                <a className="nav-link fw-bold">Về chúng tôi</a>
-                <button className="btn btn-danger"><a href="http://localhost:3000/login">Đăng nhập</a></button>
-            </section>
-        </nav>
-    );
-}
-
-
-const Default = () => {
+const Navbar = () => {
     const { data: session } = useSession();
     const userImage = session?.user?.image ? (
         <Image
@@ -88,7 +71,4 @@ const Default = () => {
 }
 
 
-export const Navbar = {
-    Specical,
-    Default
-};
+export default Navbar;
