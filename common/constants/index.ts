2311f93aaa35b1c8cf09b1abb409174e6dac07f6
Bùi Vũ Thiên Ân (anbvt) <anbvt562003@gmail.com>
2023-09-22 18:33:37 +0700

    Update Design CardDiscount

--- common/constants/index.ts
+++ common/constants/index.ts
@@ -1,3 +1,4 @@
 export const constants = {
-    URL_IMAGES: "http://localhost:8080/images/movie/"
+    URL_IMAGES: "http://localhost:8080/images/movie/",
+    ROOT: "http://localhost:8080/"
 }
---
+++ components/CardDiscount/index.tsx
@@ -0,0 +1,31 @@
+import { constants } from "@/common/constants"
+import { QRCode } from "antd"
+import Image from "next/image"
+import Link from "next/link"
+
+type CardDiscount = {
+    discount: string,
+    images: string,
+    to: string
+}
+
+export const CardDiscount = ({ ...props }: CardDiscount) => {
+    return (
+        <div className="text-center font-semibold w-full text-lg">
+            <h1 className="text-white mb-5"><span className="border-b-2 border-red-500 pb-2">KHUYẾN MÃI {props.discount}</span></h1>
+            <div className="bg-red-950 p-2 mb-4 hover:bg-red-500">
+                <Link href={props.to}>
+                    <h4 className="text-white">ĐĂNG KÍ NGAY</h4>
+                </Link>
+            </div>
+            <QRCode
+                className="mx-auto mt-4"
+                color="black"
+                bgColor="white"
+                errorLevel="H"
+                value={`${props.to}`}
+                icon={`${constants.ROOT}images/logo.jpg`}
+            />
+        </div>
+    )
+}
