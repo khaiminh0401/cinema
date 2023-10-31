01cbf340c0d759790a01b9a8cdd8c936cdaa4371
nguyễn sơn trung kiên <nguyentrungkien08010801@gmail.com>
2023-10-31 16:17:48 +0000

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,7 @@ import withAuth from "next-auth/middleware";
 
 export default withAuth(
     {
-        secret: process.env.NEXTAUTH_SECRET,
+        secret:'seocranet',
     }
 );
 export const config = {matcher: ["/book/:path*", "/user/:path*"]}
