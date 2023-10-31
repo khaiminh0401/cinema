3112fe0989589157f75c64b4c01f176762370122
thanhson <thanhson9112@gmail.com>
2023-10-31 23:05:36 +0700

    Add secret

--- cypress/e2e/login.cy.ts
+++ cypress/e2e/login.cy.ts
@@ -2,7 +2,7 @@ describe('Login Page', () => {
   it('Login', () => {
     cy.visit('/login')
     cy.get('input[type="email"]').type("doan@gmail.com")
-    cy.get('input[type="password"]').type("123456789")
+    cy.get('input[type="password"]').type("12345678")
     cy.get('button').click()
   })
 })
