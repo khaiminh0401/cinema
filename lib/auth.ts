7240cbb50a5da2976d678948998af58f40c9a150
thanhson <thanhson9112@gmail.com>
2023-10-31 23:37:27 +0700

    change env 2

--- lib/auth.ts
+++ lib/auth.ts
@@ -47,7 +47,6 @@ export const authconfig: NextAuthOptions = {
     pages: {
         signIn: '/login'
     },
-    secret: process.env.NEXTAUTH_SECRET as string,
     callbacks: {
         async jwt({user, token, session, trigger}) {
             if (trigger === "update") {
