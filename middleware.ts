84d73e963a685761b89ebc4629b3cac216f99e47
nguyễn sơn trung kiên <nguyentrungkien08010801@gmail.com>
2023-10-31 17:00:07 +0000

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,7 @@ import withAuth from "next-auth/middleware";
 
 export default withAuth(
     {
-        secret: process.env.NEXTAUTH_SECRET as string,
+        secret: process.env.NEXTAUTH_SECRET,
     }
 );
 export const config = {matcher: ["/book/:path*", "/user/:path*"]}
