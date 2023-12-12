d7e2fc0d53e81d62adcc56bb5e0960526c6ce73d
dkminh.hcm <dkminh.hcm@vnpt.vn>
2023-12-12 21:57:30 +0700

    xóa console.log

--- app/(login)/login/page.tsx
+++ app/(login)/login/page.tsx
@@ -19,11 +19,10 @@ const Login = () => {
     const session = useSession();
     if (session.data) return redirect("/");
     const onSubmit = async (data: any) => {
-        console.log(data);
         const result = await signIn("credentials", {
             email: data.email,
             password: data.password,
-            redirect: false
+            redirect: true
         });
         if (result?.error) {
             errorNotification(result.error)
