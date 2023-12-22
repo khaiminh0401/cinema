b3f4ef13d76df9eafd91a1f25db4e037d07993c9
Hoang Khang Ho <hohoangkhang18@gmail.com>
2023-12-22 01:40:34 +0000

    Update index.ts

--- common/constants/index.ts
+++ common/constants/index.ts
@@ -1,7 +1,7 @@
 export const constants = {
     URL_IMAGES: "https://zuhot-cinema-images.s3.amazonaws.com/poster-movie/",
-    ROOT_API: "http://localhost:8080/",
-    ROOT_FE: "http://localhost:3000/",
+    ROOT_API: "https://test.zuhot-api.id.vn/",
+    ROOT_FE: "https://zuhot.id.vn/",
     URL_TOPPING: "https://zuhot-cinema-images.s3.amazonaws.com/topping/",
     URL_LOGO: "https://zuhot-cinema-images.s3.amazonaws.com/logo.png",
     USD: 23000
