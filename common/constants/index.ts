6a51e785a094c34ae7a40126ab7b1bcf98384443
Thanh Sơn <thanhson9112@gmail.com>
2024-01-14 09:07:12 +0000

    Change API

--- common/constants/index.ts
+++ common/constants/index.ts
@@ -1,6 +1,6 @@
 export const constants = {
     URL_IMAGES: "https://zuhot-cinema-images.s3.amazonaws.com/poster-movie/",
-    ROOT_API: "https://test.zuhot-api.id.vn/",
+    ROOT_API: "https://ticketboxbe-production.up.railway.app/",
     ROOT_FE: "https://zuhot.id.vn/",
     URL_TOPPING: "https://zuhot-cinema-images.s3.amazonaws.com/topping/",
     URL_LOGO: "https://zuhot-cinema-images.s3.amazonaws.com/logo.png",
--- util/API/axios.ts
+++ util/API/axios.ts
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 export const fetchAPI = axios.create({
-    // baseURL: "http://localhost:8080/api",
-    baseURL: "https://test.zuhot-api.id.vn/api",
+    baseURL: "https://ticketboxbe-production.up.railway.app/api",
     headers: { 'zuhot-key': 'abc123456' }
 });
 export const fetchPaypalAPI = axios.create({
